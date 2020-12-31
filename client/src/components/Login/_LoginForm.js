4824fe43265a8f3ab32b5f704a4961efd232ec35
Mudassar Tousif <tousifmudassar@gmail.com>
2020-12-31 22:34:48 +0530

    Created a Form Group Component.

---
+++ client/src/components/Login/_LoginForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class LoginForm extends Component {
+  render() {
+    return <form className="LoginForm"></form>;
+  }
+}
+
+export default LoginForm;
