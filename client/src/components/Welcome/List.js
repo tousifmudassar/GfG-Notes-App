cdc9a174b1162ac9edef16818d2806c018164170
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-03 01:00:54 +0530

    Converted Welcome into Class Component.

--- client/src/components/Welcome/List.js
+++ client/src/components/Welcome/List.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-function List({ Notes }) {
+function List({ Notes, setCurrentNote, CurrentNote }) {
   return (
     <>
       <h3>Notes List</h3>
       <ul className="list-group">
         {Notes.map((item, key) => (
-          <li className="list-group-item list-group-item-action" key={key}>
+          <li
+            className={
+              "list-group-item list-group-item-action" +
+              (CurrentNote === key ? "active" : "")
+            }
+            key={key}
+            onClick={() => setCurrentNote(item)}
+          >
             {item}
           </li>
         ))}
--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import List from "./List";
 
-const Welcome = ({ User, handleLogout }) => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="border rounded p-2">
-            <p className="m-0">Welcome, {User.Name}.</p>
-            <button className="btn btn-danger btn-sm" onClick={handleLogout}>
-              Logout
-            </button>
+class Welcome extends Component {
+  state = {
+    Notes: ["Note 1", "Note 2", "Note 3", "Note 4"],
+    CurrentNote: null
+  };
+  setCurrentNote = CurrentNote => {
+    this.setState({
+      CurrentNote
+    });
+  };
+  render() {
+    const { User, handleLogout } = this.props;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="border rounded p-2">
+              <p className="m-0">Welcome, {User.Name}.</p>
+              <button className="btn btn-danger btn-sm" onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="row mt-3">
-        <div className="col-3">
-          <List Notes={["Note 1", "Note 2", "Note 3", "Note 4"]} />
+        <div className="row mt-3">
+          <div className="col-3">
+            <List
+              Notes={this.state.Notes}
+              CurrentNote={this.state.CurrentNote}
+              setCurrentNote={this.setCurrentNote}
+            />
+          </div>
+          <div className="col-9"></div>
         </div>
-        <div className="col-9"></div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default Welcome;
