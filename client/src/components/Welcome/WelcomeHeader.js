a0e1d49e05d1f0fe88329e7a9adf7698ef625ab6
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-03 13:20:33 +0530

    Componentised Welcome Header.

---
+++ client/src/components/Welcome/WelcomeHeader.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const WelcomeHeader = ({ User, handleLogout }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="border rounded p-2">
+            <p className="m-0">Welcome, {User.Name}.</p>
+            <button className="btn btn-danger btn-sm" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeHeader;
