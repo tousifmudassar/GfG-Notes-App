3bbf262c45d14e0305381c35fcb7990a9d1a62a6
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-07 14:30:03 +0530

    Created user API endpoints.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,8 @@
 const app = require("express").Router();
+const user = require("./user")
 
 app.get("/", (req, res) => {
   res.json("Welcome to /api!");
 });
-
+app.use("/user", user);
 module.exports = app;
