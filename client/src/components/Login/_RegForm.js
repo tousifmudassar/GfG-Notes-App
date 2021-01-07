2df385f958540c51a1ac390e1cdee0d4f7b4213b
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-07 20:49:09 +0530

    Created a Registration form similiar to login.

---
+++ client/src/components/Login/_RegForm.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export class RegForm extends Component {
+  state = {
+    RegUsername: "",
+    RegPassword: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleReg(this.state.RegUsername, this.state.RegPassword);
+  };
+  render() {
+    return (
+      <form className="LoginForm" onSubmit={this.handleSubmit}>
+        <h3 className="mb-3">Register</h3>
+        {this.props.Error && (
+          <div className="alert alert-danger">{this.props.Error}</div>
+        )}
+        {this.props.Success && (
+          <div className="alert alert-success">{this.props.Success}</div>
+        )}
+        {[
+          {
+            Id: "RegUsername",
+            Type: "text",
+            Desc: "Put your username in a good case."
+          },
+          {
+            Id: "RegPassword",
+            Type: "password",
+            Desc: "Create a new password and forget it."
+          }
+        ].map((fg, key) => (
+          <FormGroup
+            {...fg}
+            Value={this.state[fg.Id]}
+            Label={fg.Id.replace("Reg", "")}
+            key={key}
+            onChange={this.handleChange}
+          />
+        ))}
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+    );
+  }
+}
+
+export default RegForm;
