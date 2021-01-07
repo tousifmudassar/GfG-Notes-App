2b7cd85ae7b4fe1ca932c16405909f164ecdca66
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-07 15:32:26 +0530

    Created User Route with our sample users.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,41 @@
-const app = require("express").Router();
+const user = require("express").Router();
 
-app.get("/", (req, res) => {
-  res.json("Welcome to /api/user!");
+//Dummy list of users for now.
+const Users = {
+  Mudassar: "123",
+  Praveen: "Hello123"
+};
+
+user.get("/", (req, res) => {
+  res.json(req.session.User);
 });
-app.get("/");
-module.exports = app;
+
+user.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!Users[username]) {
+    //User not found
+    req.session.destroy();
+    res.status(404).json({
+      Error: true,
+      Success: false,
+      Message: "User not found!"
+    });
+  } else if (Users[username] && Users[username] !== password) {
+    //password is wrong
+    req.session.destroy();
+    res.status(403).json({
+      Error: true,
+      Success: false,
+      Message: "Invalid username and password!"
+    });
+  } else {
+    //password is right.
+    res.json({
+      Error: false,
+      Success: true,
+      Message: { Name: username }
+    });
+  }
+});
+
+module.exports = user;
