1c2185ff4001e65f4791e39e56cccdb1130faf8a
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-07 14:16:29 +0530

    Created a route for API.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const api = require("./routes/api");
 const app = express();
 const port = 3100;
 
@@ -10,6 +11,7 @@ app.get("/", (req, res) => {
   res.json("Welcome to API Server!");
 });
 app.use("/api", api);
+
 app.listen(port, () => {
   console.log(`Server started in port ${port}!`);
 });
