7e81f7fd836bf5890ddfb61683fd815f8ffb38bb
Mudassar Tousif <tousifmudassar@gmail.com>
2021-01-07 23:38:24 +0530

    Added gaurd condition for handling missing notes.

--- client/src/components/Welcome/_NoteContent.js
+++ client/src/components/Welcome/_NoteContent.js
@@ -1,11 +1,34 @@
 import React from "react";
+import MDEditor from "@uiw/react-md-editor";
 
 const NoteContent = ({ match, Notes }) => {
-  const { Title, Desc } = Notes[+match.params.NoteID.replace("note-", "")];
+  if (Notes.length === 0) {
+    return (
+      <>
+        <h3 className="mb-3">Loading...</h3>
+        <p>Please Wait...</p>
+      </>
+    );
+  }
+  const Note = Notes.find(n => n.NoteID === match.params.NoteID);
+  if (!Note) {
+    return (
+      <>
+        <h3 className="mb-3">Note Not Found!</h3>
+        <p>Whoops! The Note you're looking for is not found! 😔</p>
+      </>
+    );
+  }
+  const { NoteID, Title, Desc, User } = Note;
   return (
     <>
-      <h3 className="mb-3">{Title}</h3>
-      <p>{Desc}</p>
+      <h3 className="mb-3">
+        {Title} <code>({NoteID})</code>
+      </h3>
+      <p>
+        <em>Created by {User}.</em>
+      </p>
+      <MDEditor.Markdown source={Desc} />
     </>
   );
 };
